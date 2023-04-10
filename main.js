d91c8db5ad495a4395c254840ee774e7167758f5
Zaxoosh <49976234+Zaxoosh@users.noreply.github.com>
2023-04-10 23:15:25 +0100

    Update main.js
    persistent timing test 2

--- main.js
+++ main.js
@@ -1,64 +1,52 @@
-// Import LocalStorageDB library
+<!-- Include the LocalStorageDB library -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/localStorageDB/2.7/localStorageDB.min.js"></script>
 
+<!-- Create an element to display the clock -->
+<div id="clock"></div>
+
+<script>
+// Create a LocalStorageDB instance
+const db = new localStorageDB('myDB', localStorage);
+
+// Define a function to update the time
 function updateTime() {
-  const startDate = new Date();
-  const clock = document.getElementById("clock");
+  let startDate = new Date();
+  let clock = document.getElementById("clock");
+  setInterval(() => {
+    // Fetch current date and time from an API
+    fetch('https://worldtimeapi.org/api/timezone/Etc/UTC')
+      .then(response => response.json())
+      .then(({ datetime }) => {
+        let currentDate = new Date(datetime);
+        let diff = currentDate.getTime() - startDate.getTime();
+        let totalSeconds = Math.floor(diff / 1000);
+        let days = Math.floor(totalSeconds / (60 * 60 * 24));
+        let hours = Math.floor((totalSeconds % (60 * 60 * 24)) / (60 * 60));
+        let minutes = Math.floor((totalSeconds % (60 * 60)) / 60);
+        let seconds = totalSeconds % 60;
+        let timeString = `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`;
+        clock.innerHTML = timeString;
 
-  // Check if LocalStorageDB is supported
-  if (localStorageDB.isSupported()) {
-    // Initialize or open LocalStorageDB database
-    const db = new localStorageDB("myDB", localStorage);
+        // Store the time data in LocalStorageDB
+        db.insertOrUpdate('timeData', { id: 1, time: timeString });
 
-    // Check if database is initialized successfully
-    if (db.isNew()) {
-      // Create a new table to store time data
-      db.createTable("timeData", ["dateString"]);
-    }
-
-    setInterval(() => {
-      fetch('https://timeapi.io/api/Time/current/zone?timeZone=Etc/UTC', { mode: 'no-cors' })
-        .then(response => response.json())
-        .then(({ dateString }) => {
-          const currentDate = new Date(dateString);
-          const diff = currentDate.getTime() - startDate.getTime();
-          const totalSeconds = Math.floor(diff / 1000);
-          const days = Math.floor(totalSeconds / (60 * 60 * 24));
-          const hours = Math.floor((totalSeconds % (60 * 60 * 24)) / (60 * 60));
-          const minutes = Math.floor((totalSeconds % (60 * 60)) / 60);
-          const seconds = totalSeconds % 60;
-          clock.innerHTML = `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`;
-
-          // Store the time data in LocalStorageDB
-          db.insert("timeData", { dateString });
-          db.commit();
-        })
-        .catch(error => {
-          console.error('Error:', error);
-        });
-    }, 1000);
-  } else {
-    // Fallback to non-persistent version
-    setInterval(() => {
-      fetch('https://timeapi.io/api/Time/current/zone?timeZone=Etc/UTC', { mode: 'no-cors' })
-        .then(response => response.json())
-        .then(({ dateString }) => {
-          const currentDate = new Date(dateString);
-          const diff = currentDate.getTime() - startDate.getTime();
-          const totalSeconds = Math.floor(diff / 1000);
-          const days = Math.floor(totalSeconds / (60 * 60 * 24));
-          const hours = Math.floor((totalSeconds % (60 * 60 * 24)) / (60 * 60));
-          const minutes = Math.floor((totalSeconds % (60 * 60)) / 60);
-          const seconds = totalSeconds % 60;
-          clock.innerHTML = `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`;
-        })
-        .catch(error => {
-          console.error('Error:', error);
-        });
-    }, 1000);
-  }
+        // Commit changes to LocalStorageDB
+        db.commit();
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }, 1000);
 }
 
+// Check if time data exists in LocalStorageDB and display it
+if (db.tableExists('timeData')) {
+  let timeData = db.queryAll('timeData')[0];
+  document.getElementById("clock").innerHTML = timeData.time;
+}
+
+// Call updateTime function on window load
 window.onload = () => {
   updateTime();
 };
+</script>
