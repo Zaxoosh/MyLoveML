26fa8658ccd002fae5b9bfa1b5d8376824c68aaa
Zaxoosh <49976234+Zaxoosh@users.noreply.github.com>
2023-04-10 23:10:58 +0100

    Update main.js
    New persistent clock test

--- main.js
+++ main.js
@@ -1,23 +1,62 @@
+// Import LocalStorageDB library
+<script src="https://cdnjs.cloudflare.com/ajax/libs/localStorageDB/2.7/localStorageDB.min.js"></script>
+
 function updateTime() {
   const startDate = new Date();
   const clock = document.getElementById("clock");
-  setInterval(() => {
-    fetch('https://worldtimeapi.org/api/timezone/Etc/UTC')
-      .then(response => response.json())
-      .then(({ utc_datetime }) => {
-        const currentDate = new Date(utc_datetime);
-        const diff = currentDate.getTime() - startDate.getTime();
-        const totalSeconds = Math.floor(diff / 1000);
-        const days = Math.floor(totalSeconds / (60 * 60 * 24));
-        const hours = Math.floor((totalSeconds % (60 * 60 * 24)) / (60 * 60));
-        const minutes = Math.floor((totalSeconds % (60 * 60)) / 60);
-        const seconds = totalSeconds % 60;
-        clock.innerHTML = `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`;
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
-  }, 1000);
+
+  // Check if LocalStorageDB is supported
+  if (localStorageDB.isSupported()) {
+    // Initialize or open LocalStorageDB database
+    const db = new localStorageDB("myDB", localStorage);
+
+    // Check if database is initialized successfully
+    if (db.isNew()) {
+      // Create a new table to store time data
+      db.createTable("timeData", ["dateString"]);
+    }
+
+    setInterval(() => {
+      fetch('https://timeapi.io/api/Time/current/zone?timeZone=Etc/UTC', { mode: 'no-cors' })
+        .then(response => response.json())
+        .then(({ dateString }) => {
+          const currentDate = new Date(dateString);
+          const diff = currentDate.getTime() - startDate.getTime();
+          const totalSeconds = Math.floor(diff / 1000);
+          const days = Math.floor(totalSeconds / (60 * 60 * 24));
+          const hours = Math.floor((totalSeconds % (60 * 60 * 24)) / (60 * 60));
+          const minutes = Math.floor((totalSeconds % (60 * 60)) / 60);
+          const seconds = totalSeconds % 60;
+          clock.innerHTML = `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`;
+
+          // Store the time data in LocalStorageDB
+          db.insert("timeData", { dateString });
+          db.commit();
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
+    }, 1000);
+  } else {
+    // Fallback to non-persistent version
+    setInterval(() => {
+      fetch('https://timeapi.io/api/Time/current/zone?timeZone=Etc/UTC', { mode: 'no-cors' })
+        .then(response => response.json())
+        .then(({ dateString }) => {
+          const currentDate = new Date(dateString);
+          const diff = currentDate.getTime() - startDate.getTime();
+          const totalSeconds = Math.floor(diff / 1000);
+          const days = Math.floor(totalSeconds / (60 * 60 * 24));
+          const hours = Math.floor((totalSeconds % (60 * 60 * 24)) / (60 * 60));
+          const minutes = Math.floor((totalSeconds % (60 * 60)) / 60);
+          const seconds = totalSeconds % 60;
+          clock.innerHTML = `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`;
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
+    }, 1000);
+  }
 }
 
 window.onload = () => {
