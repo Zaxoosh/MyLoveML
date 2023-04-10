c8247059e918453e91ecf227b12ca4629820057d
Zaxoosh <49976234+Zaxoosh@users.noreply.github.com>
2023-04-10 22:27:37 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,27 +1,25 @@
-function updateTime() { 
-  var startDate = new Date(); 
-  var clock = document.getElementById('clock'); 
-  setInterval(function() { 
-    fetch('https://timeapi.io/api/Time/current/zone?timeZone=Etc/UTC') 
-      .then(function(response) { 
-        return response.json(); 
-      }) 
-      .then(function(data) { 
-        var currentDate = new Date(data.dateString); 
-        var diff = currentDate.getTime() - startDate.getTime(); 
-        var totalSeconds = Math.floor(diff / 1000); 
-        var days = Math.floor(totalSeconds / (60 * 60 * 24)); 
-        var hours = Math.floor((totalSeconds % (60 * 60 * 24)) / (60 * 60)); 
-        var minutes = Math.floor((totalSeconds % (60 * 60)) / 60); 
-        var seconds = totalSeconds % 60; 
-        clock.innerHTML = days + ' days, ' + hours + ' hours, ' + minutes + ' minutes, ' + seconds + ' seconds'; 
-      }) 
-      .catch(function(error) { 
-        console.error('Error:', error); 
-      }); 
-  }, 1000); 
-} 
+function updateTime() {
+  const startDate = new Date();
+  const clock = document.getElementById('clock');
+  setInterval(() => {
+    fetch('https://timeapi.io/api/Time/current/zone?timeZone=Etc/UTC')
+      .then(response => response.json())
+      .then(({ dateString }) => {
+        const currentDate = new Date(dateString);
+        const diff = currentDate.getTime() - startDate.getTime();
+        const totalSeconds = Math.floor(diff / 1000);
+        const days = Math.floor(totalSeconds / (60 * 60 * 24));
+        const hours = Math.floor((totalSeconds % (60 * 60 * 24)) / (60 * 60));
+        const minutes = Math.floor((totalSeconds % (60 * 60)) / 60);
+        const seconds = totalSeconds % 60;
+        clock.innerHTML = `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`;
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }, 1000);
+}
 
-document.addEventListener('DOMContentLoaded', function() { 
-  updateTime(); // Call the function on DOMContentLoaded 
+document.addEventListener('DOMContentLoaded', () => {
+  updateTime();
 });
