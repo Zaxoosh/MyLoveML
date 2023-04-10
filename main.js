7166643de13bcd2a17673cf46d0c63e558cacee2
Zaxoosh <49976234+Zaxoosh@users.noreply.github.com>
2023-04-10 23:19:09 +0100

    Update main.js
    perm time test 3

--- main.js
+++ main.js
@@ -12,31 +12,30 @@ const db = new localStorageDB('myDB', localStorage);
 function updateTime() {
   let startDate = new Date();
   let clock = document.getElementById("clock");
-  setInterval(() => {
-    // Fetch current date and time from an API
-    fetch('https://worldtimeapi.org/api/timezone/Etc/UTC')
-      .then(response => response.json())
-      .then(({ datetime }) => {
-        let currentDate = new Date(datetime);
-        let diff = currentDate.getTime() - startDate.getTime();
-        let totalSeconds = Math.floor(diff / 1000);
-        let days = Math.floor(totalSeconds / (60 * 60 * 24));
-        let hours = Math.floor((totalSeconds % (60 * 60 * 24)) / (60 * 60));
-        let minutes = Math.floor((totalSeconds % (60 * 60)) / 60);
-        let seconds = totalSeconds % 60;
-        let timeString = `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`;
-        clock.innerHTML = timeString;
+  
+  // Fetch current date and time from an API
+  fetch('https://worldtimeapi.org/api/timezone/Etc/UTC')
+    .then(response => response.json())
+    .then(({ datetime }) => {
+      let currentDate = new Date(datetime);
+      let diff = currentDate.getTime() - startDate.getTime();
+      let totalSeconds = Math.floor(diff / 1000);
+      let days = Math.floor(totalSeconds / (60 * 60 * 24));
+      let hours = Math.floor((totalSeconds % (60 * 60 * 24)) / (60 * 60));
+      let minutes = Math.floor((totalSeconds % (60 * 60)) / 60);
+      let seconds = totalSeconds % 60;
+      let timeString = `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`;
+      clock.innerHTML = timeString;
 
-        // Store the time data in LocalStorageDB
-        db.insertOrUpdate('timeData', { id: 1, time: timeString });
+      // Store the time data in LocalStorageDB
+      db.insertOrUpdate('timeData', { id: 1, time: timeString });
 
-        // Commit changes to LocalStorageDB
-        db.commit();
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
-  }, 1000);
+      // Commit changes to LocalStorageDB
+      db.commit();
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
 }
 
 // Check if time data exists in LocalStorageDB and display it
@@ -48,5 +47,6 @@ if (db.tableExists('timeData')) {
 // Call updateTime function on window load
 window.onload = () => {
   updateTime();
+  setInterval(updateTime, 1000); // Call updateTime function every 1000ms (1 second)
 };
 </script>
