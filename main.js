76bda0af10953e4e704d53a1e4c8afce6401a4d7
Zaxoosh <49976234+Zaxoosh@users.noreply.github.com>
2023-04-10 23:03:44 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
-function updateTime() { 
-  const startDate = new Date(); 
-  const clock = document.getElementById("clock"); 
-  setInterval(() => { 
-    fetch('https://timeapi.io/api/Time/current/zone?timeZone=Etc/UTC', {mode: 'no-cors'}) 
-      .then(response => response.json()) 
-      .then(({ dateString }) => { 
-        const currentDate = new Date(dateString); 
-        const diff = currentDate.getTime() - startDate.getTime(); 
-        const totalSeconds = Math.floor(diff / 1000); 
-        const days = Math.floor(totalSeconds / (60 * 60 * 24)); 
-        const hours = Math.floor((totalSeconds % (60 * 60 * 24)) / (60 * 60)); 
-        const minutes = Math.floor((totalSeconds % (60 * 60)) / 60); 
-        const seconds = totalSeconds % 60; 
-        clock.innerHTML = `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`; 
-      }) 
-      .catch(error => { 
-        console.error('Error:', error); 
-      }); 
-  }, 1000); 
-} 
+function updateTime() {
+  const startDate = new Date();
+  const clock = document.getElementById("clock");
+  setInterval(() => {
+    fetch('https://worldtimeapi.org/api/timezone/Etc/UTC')
+      .then(response => response.json())
+      .then(({ utc_datetime }) => {
+        const currentDate = new Date(utc_datetime);
+        const diff = currentDate.getTime() - startDate.getTime();
+        const totalSeconds = Math.floor(diff / 1000);
+        const days = Math.floor(totalSeconds / (60 * 60 * 24));
+        const hours = Math.floor((totalSeconds % (60 * 60 * 24)) / (60 * 60));
+        const minutes = Math.floor((totalSeconds % (60 * 60)) / 60);
+        const seconds = totalSeconds % 60;
+        clock.innerHTML = `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`;
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }, 1000);
+}
 
-window.onload = () => { 
-  updateTime(); 
+window.onload = () => {
+  updateTime();
 };
