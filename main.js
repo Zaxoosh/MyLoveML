e70d5f88b9beb6e34a9e8949f664c32d97d8568e
Zaxoosh <49976234+Zaxoosh@users.noreply.github.com>
2023-04-10 22:36:02 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,19 @@
-function updateTime() {
-  const startDate = new Date();
-  const clock = document.getElementById('clock');
-  setInterval(() => {
-    fetch('https://timeapi.io/api/Time/current/zone?timeZone=Etc/UTC')
-      .then(response => response.json())
-      .then(({ dateString }) => {
-        const currentDate = new Date(dateString);
-        const diff = currentDate.getTime() - startDate.getTime();
-        const totalSeconds = Math.floor(diff / 1000);
-        const days = Math.floor(totalSeconds / (60 * 60 * 24));
-        const hours = Math.floor((totalSeconds % (60 * 60 * 24)) / (60 * 60));
-        const minutes = Math.floor((totalSeconds % (60 * 60)) / 60);
-        const seconds = totalSeconds % 60;
-        clock.innerHTML = `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`;
+function updateTime() { 
+  const startDate = new Date(); 
+  const clock = document.getElementById('clock'); 
+  setInterval(() => { 
+    fetch('https://timeapi.io/api/Time/current/zone?timeZone=Etc/UTC', { mode: 'no-cors' }) // Add 'mode: no-cors' to the fetch options
+      .then(response => response.text()) // Note: response will be opaque, so only text() method can be used
+      .then(data => {
+        // Process the response data
+        // Note: Since the response is opaque, you won't be able to access the response body or headers
       })
-      .catch(error => {
-        console.error('Error:', error);
-      });
-  }, 1000);
-}
+      .catch(error => { 
+        console.error('Error:', error); 
+      }); 
+  }, 1000); 
+} 
 
-document.addEventListener('DOMContentLoaded', () => {
-  updateTime();
+document.addEventListener('DOMContentLoaded', () => { 
+  updateTime(); 
 });
