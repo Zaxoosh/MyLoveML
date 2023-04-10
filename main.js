cdc6004cc210ea09a4a97c6cf2f2c080d541b9c4
Zaxoosh <49976234+Zaxoosh@users.noreply.github.com>
2023-04-10 22:20:55 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,27 +1,27 @@
-function updateTime() {
-  var startDate = new Date();
-  var clock = document.getElementById('clock');
-  setInterval(function() {
-    fetch('https://timeapi.io/api/Time/current/zone?timeZone=Etc/UTC')
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(data) {
-        var currentDate = new Date(data.dateString);
-        var diff = currentDate.getTime() - startDate.getTime();
-        var totalSeconds = Math.floor(diff / 1000);
-        var days = Math.floor(totalSeconds / (60 * 60 * 24));
-        var hours = Math.floor((totalSeconds % (60 * 60 * 24)) / (60 * 60));
-        var minutes = Math.floor((totalSeconds % (60 * 60)) / 60);
-        var seconds = totalSeconds % 60;
-        clock.innerHTML = days + ' days, ' + hours + ' hours, ' + minutes + ' minutes, ' + seconds + ' seconds';
-      })
-      .catch(function(error) {
-        console.error('Error:', error);
-      });
-  }, 1000);
-}
+function updateTime() { 
+  var startDate = new Date(); 
+  var clock = document.getElementById('clock'); 
+  setInterval(function() { 
+    fetch('https://timeapi.io/api/Time/current/zone?timeZone=Etc/UTC') 
+      .then(function(response) { 
+        return response.json(); 
+      }) 
+      .then(function(data) { 
+        var currentDate = new Date(data.dateString); 
+        var diff = currentDate.getTime() - startDate.getTime(); 
+        var totalSeconds = Math.floor(diff / 1000); 
+        var days = Math.floor(totalSeconds / (60 * 60 * 24)); 
+        var hours = Math.floor((totalSeconds % (60 * 60 * 24)) / (60 * 60)); 
+        var minutes = Math.floor((totalSeconds % (60 * 60)) / 60); 
+        var seconds = totalSeconds % 60; 
+        clock.innerHTML = days + ' days, ' + hours + ' hours, ' + minutes + ' minutes, ' + seconds + ' seconds'; 
+      }) 
+      .catch(function(error) { 
+        console.error('Error:', error); 
+      }); 
+  }, 1000); 
+} 
 
-window.onload = function() {
-  updateTime(); // Call the function on page load
-};
+document.addEventListener('DOMContentLoaded', function() { 
+  updateTime(); // Call the function on DOMContentLoaded 
+});
