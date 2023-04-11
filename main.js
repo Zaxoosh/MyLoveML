bdf6d90a1abd43224f080ec31e6a0dc97dfe7cf3
Zaxoosh <49976234+Zaxoosh@users.noreply.github.com>
2023-04-11 11:27:50 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,39 +1,33 @@
-// Function to format time data as a string
-function formatTimeData(timeData) {
-  let formattedTime = '';
-  if (timeData.days > 0) {
-    formattedTime += 'Days: ' + timeData.days + ' ';
-  }
-  if (timeData.hours > 0 || timeData.days > 0) {
-    formattedTime += 'Hours: ' + String(timeData.hours).padStart(2, '0') + ' ';
-  }
-  if (timeData.minutes > 0 || timeData.hours > 0 || timeData.days > 0) {
-    formattedTime += 'Minutes: ' + String(timeData.minutes).padStart(2, '0') + ' ';
-  }
-  formattedTime += 'Seconds: ' + String(timeData.seconds).padStart(2, '0');
-  return formattedTime;
+// JavaScript code for clock
+function updateClock() {
+  var now = new Date();
+  var hours = String(now.getHours()).padStart(2, '0');
+  var minutes = String(now.getMinutes()).padStart(2, '0');
+  var seconds = String(now.getSeconds()).padStart(2, '0');
+  var timeString = hours + ':' + minutes + ':' + seconds;
+  document.getElementById('clock').textContent = timeString;
+  setTimeout(updateClock, 1000);
 }
+updateClock();
 
-// Define the date when you met in UTC format
-const startDate = Date.UTC(2022, 9, 30, 0, 0, 0); // Month is zero-based (0 for January)
-
-// Function to update the time
-function updateTime() {
-  let clock = document.getElementById("clock");
-  let currentDate = new Date();
-  let timeDifference = Math.floor((currentDate.getTime() - startDate) / 1000);
-  let timeData = {
-    days: Math.floor(timeDifference / (60 * 60 * 24)),
-    hours: Math.floor((timeDifference % (60 * 60 * 24)) / (60 * 60)),
-    minutes: Math.floor((timeDifference % (60 * 60)) / 60),
-    seconds: timeDifference % 60
-  };
-  let timeString = formatTimeData(timeData);
-  clock.innerHTML = timeString;
+// Update days, hours, minutes, and seconds in the clock
+function updateCountdown() {
+  var countdownDate = new Date('2023-04-08T00:00:00Z'); // Set your target date and time here
+  var now = new Date().getTime();
+  var distance = countdownDate - now;
+  
+  // Calculate days, hours, minutes, and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  
+  // Update the countdown elements in the HTML
+  document.getElementById('days').textContent = days;
+  document.getElementById('hours').textContent = hours;
+  document.getElementById('minutes').textContent = minutes;
+  document.getElementById('seconds').textContent = seconds;
+  
+  setTimeout(updateCountdown, 1000);
 }
-
-// Call updateTime function on window load
-window.onload = () => {
-  updateTime();
-  setInterval(updateTime, 1000); // Call updateTime function every 1000ms (1 second)
-};
+updateCountdown();
